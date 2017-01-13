a7eca414de10fd36a63b14f221769ee0254f2df4
Eliran Levi <eliran@stratoscale.com>
2017-01-14 00:29:51 +0200

    Make sure VIM has python support

--- setup.sh
+++ setup.sh
@@ -125,6 +125,12 @@ fi
 if [ ! -d "$HOME/.vim/bundle/vim-surround" ]; then
     git clone https://github.com/nvie/vim-surround ~/.vim/bundle/vim-surround
 fi
+log "Making sure VIM has python support..."
+# taken from http://askubuntu.com/questions/775059/vim-python-support-on-ubuntu-16-04
+add-apt-repository -y ppa:pi-rho/dev
+apt-get -y update
+apt-get install -y vim-gtk
+
 log "Copying flake8 configuration file..."
 mkdir -p ~/.config
 cp {,~/.config/}flake8
